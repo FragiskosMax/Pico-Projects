4eb5bcd2b661e1bbc0be1076d42e0d95aa55c634
FragiskosMax <159783102+FragiskosMax@users.noreply.github.com>
2024-02-12 17:41:05 +0200

    Update blinky.py

--- blinky.py
+++ blinky.py
@@ -1,9 +1,9 @@
-import machine
-import utime
-led = machine.Pin("LED", machine.Pin.OUT)
+import machine # We import a library to cntrol our Pico
+import utime # We import another library to control the time
+led = machine.Pin("LED", machine.Pin.OUT) # We set Pico's led
 
-while True:
-    led.off()
-    utime.sleep(0.5)
-    led.on()
-    utime.sleep(0.5)
+while True: # Forever (You can change it if you want)
+    led.off() # Close the led
+    utime.sleep(0.5) # Wait 0.5 sec
+    led.on() # Open the led
+    utime.sleep(0.5) # Wait 0.5 sec
